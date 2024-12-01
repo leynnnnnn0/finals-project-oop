3d0336b5764deefe572a6150137d3edc5b30c8cb
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-12-01 22:05:23 +0800

    first commit

---
+++ src/AppController.java
@@ -0,0 +1,79 @@
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class AppController implements Initializable {
+    public MFXButton homeSidebarText;
+    public MFXButton residentsSidebarText;
+    public MFXButton usersSidebarText;
+    @FXML
+    private Label exit;
+
+    @FXML
+    private StackPane contentArea;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        exit.setOnMouseClicked(e -> {
+            System.exit(0);
+        });
+        try {
+            Parent fxml = FXMLLoader.load(getClass().getResource("home.fxml"));
+            contentArea.getChildren().removeAll();
+            contentArea.getChildren().setAll(fxml);
+        } catch (IOException e) {
+            Logger.getLogger(ModuleLayer.Controller.class.getName()).log(Level.SEVERE, null, e);
+        }
+
+        resetButtonStyles();
+        homeSidebarText.setStyle("-fx-text-fill: #2D60FF; -fx-font-weight: bold;");
+
+    }
+
+    private void resetButtonStyles() {
+        homeSidebarText.setStyle("-fx-text-fill: #b1b1b1; -fx-font-weight: bold;");
+        residentsSidebarText.setStyle("-fx-text-fill: #b1b1b1; -fx-font-weight: bold;");
+        usersSidebarText.setStyle("-fx-text-fill: #b1b1b1; -fx-font-weight: bold;");
+    }
+
+    public void home(javafx.event.ActionEvent actionEvent) throws IOException {
+        resetButtonStyles();
+        homeSidebarText.setStyle("-fx-text-fill: #2D60FF; -fx-font-weight: bold;");
+
+        Parent fxml = FXMLLoader.load(getClass().getResource("home.fxml"));
+        contentArea.getChildren().removeAll();
+        contentArea.getChildren().setAll(fxml);
+    }
+
+    public void residents(javafx.event.ActionEvent actionEvent) throws IOException {
+        resetButtonStyles();
+        residentsSidebarText.setStyle("-fx-text-fill: #2D60FF; -fx-font-weight: bold;");
+
+        Parent fxml = FXMLLoader.load(getClass().getResource("residents.fxml"));
+        contentArea.getChildren().removeAll();
+        contentArea.getChildren().setAll(fxml);
+    }
+
+    public void users(javafx.event.ActionEvent actionEvent) throws IOException {
+        resetButtonStyles();
+        usersSidebarText.setStyle("-fx-text-fill: #2D60FF; -fx-font-weight: bold;");
+
+        Parent fxml = FXMLLoader.load(getClass().getResource("user.fxml"));
+        contentArea.getChildren().removeAll();
+        contentArea.getChildren().setAll(fxml);
+    }
+
+
+
+
+}

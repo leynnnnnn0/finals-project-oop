44089ab3fa934275c12bd05857b2a177afebc8c4
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-12-01 23:58:20 +0800

    added initial fields for creating a resident

--- src/Resident.java
+++ src/Resident.java
@@ -1,2 +1,40 @@
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXComboBox;
+import io.github.palexdev.materialfx.controls.MFXFilterComboBox;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Resident {
+
+    public AnchorPane residentIndexPane;
+    public AnchorPane residentCreatePane;
+    public MFXComboBox<String> genderComboBox;
+    public MFXFilterComboBox<String> nationalityComboBox;
+
+    public void initialize() {
+        ObservableList<String> genderOptions = FXCollections.observableArrayList();
+        genderOptions.addAll(
+                "Male",
+                "Female"
+        );
+        ObservableList<String> nationalityOptions = FXCollections.observableArrayList();
+        nationalityOptions.addAll(
+                "Filipino",
+                "American"
+        );
+        nationalityComboBox.setItems(nationalityOptions);
+       genderComboBox.setItems(genderOptions);
+    }
+
+    public void addNewResident(javafx.event.ActionEvent actionEvent) throws IOException {
+        residentIndexPane.setVisible(false);
+        residentCreatePane.setVisible(true);
+    }
+
 }

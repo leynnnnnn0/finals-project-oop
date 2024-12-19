eb4a4a0f6b76dcdbc7cacfd0d4ff6174f9efba9b
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-12-19 09:05:39 +0800

    test

--- src/ResidentProfileController.java
+++ src/ResidentProfileController.java
@@ -1,2 +1,17 @@
+import io.github.palexdev.materialfx.controls.MFXTableView;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class ResidentProfileController {
+    public AnchorPane blottersIndexPane;
+    public MFXTableView table;
+    public TextField searchField;
+
+    public void search(KeyEvent keyEvent) {
+    }
+
+    public void openCreateBlotterRequest(ActionEvent actionEvent) {
+    }
 }
